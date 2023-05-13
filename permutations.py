14d1041ddfb87e220f711ececb5e29934093fe92
Vlad <vlad.kapitsa.mail.ru@gmail.com>
2023-05-13 14:57:30 +0300

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums):
+        '''
+        Given an array nums of distinct integers,
+        returns all the possible permutations.
+        '''
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            # return # backtracking
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
