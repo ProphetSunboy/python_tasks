60eaa1985e03e70a609c243aa2642ec6354358cb
Vlad <vlad.kapitsa.mail.ru@gmail.com>
2023-05-18 15:23:02 +0300

    Create missing_number.py

---
+++ missing_number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        '''
+        Given an array nums containing n distinct numbers in the range [0, n],
+        return the only number in the range that is missing from the array.
+        '''
+        return (set(range(0, len(nums)+1)) - set(nums)).pop()
