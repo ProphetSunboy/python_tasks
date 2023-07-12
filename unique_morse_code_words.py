f19efb79afc2cbea3b43dcea832021161be11d1d
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2023-07-12 13:25:16 +0300

    Create unique_morse_code_words.py

---
+++ unique_morse_code_words.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: list[str]) -> int:
+        """
+        International Morse Code defines a standard encoding where each letter
+        is mapped to a series of dots and dashes, as follows:
+
+            'a' maps to ".-",
+            'b' maps to "-...",
+            'c' maps to "-.-.", and so on.
+
+        Given an array of strings words where each word can be written as a
+        concatenation of the Morse code of each letter.
+
+            For example, "cab" can be written as "-.-..--...", which is the
+            concatenation of "-.-.", ".-", and "-...". We will call such a
+            concatenation the transformation of a word.
+
+        Return the number of different transformations among all words we have.
+        """
+        morse = [
+                 ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+                 "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+                 "..-","...-",".--","-..-","-.--","--.."
+                ]
+
+        res = set()
+        
+        for word in words:
+            morse_s = ''
+            for ch in word:
+                morse_s += morse[ord(ch)-97]
+            res.add(morse_s)
+
+        return len(res)
