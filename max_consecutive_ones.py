54223353d44c40cf2a59750f0bfb3276fb2fc572
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2023-08-17 13:43:10 +0300

    Create max_consecutive_ones.py

---
+++ max_consecutive_ones.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+        """
+        Given a binary list nums, return the maximum number of consecutive 1's
+        in the list.
+
+        :param int nums: binary list
+        :returns int max_cons: maximum number of consecutive 1's
+
+        Time complexity: O(n)
+        Space complexity: O(1)
+
+        LeetCode: Beats 99.73%
+        """
+        max_cons = 0
+        curr = 0
+
+        for num in nums:
+            if num == 1:
+                curr += 1
+            else:
+                if curr > max_cons:
+                    max_cons = curr
+
+                curr = 0
+
+        if curr > max_cons:
+            max_cons = curr
+
+        return max_cons
