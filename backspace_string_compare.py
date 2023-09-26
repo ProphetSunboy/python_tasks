a2b3b4f96632bbe433710b19ebf3cf3940d0b7cc
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2023-09-26 13:38:48 +0300

    Create backspace_string_compare.py

---
+++ backspace_string_compare.py
@@ -0,0 +1,36 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        """
+        Given two strings s and t, return True if they are equal when both are
+        typed into empty text editors. '#' means a backspace character.
+
+        Note that after backspacing an empty text, the text will continue empty.
+
+        :param str s: string
+        :param str t: string
+        :param bool equal: s and t are equal when both are typed into empty text
+        editors
+
+        Time complexity: O(n)
+        Space complexity: O(n)
+
+        LeetCode: Beats 99.27%
+        """
+        stack_s = []
+        stack_t = []
+
+        for ch in s:
+            if ch == "#":
+                if stack_s:
+                    stack_s.pop()
+            else:
+                stack_s.append(ch)
+
+        for ch in t:
+            if ch == "#":
+                if stack_t:
+                    stack_t.pop()
+            else:
+                stack_t.append(ch)
+
+        return stack_s == stack_t
