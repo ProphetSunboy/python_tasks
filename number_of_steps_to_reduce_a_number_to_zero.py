d3c5efa453045e08d0c215bc2fd66cfdd882a637
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-05-21 12:44:53 +0300

    Update number_of_steps_to_reduce_a_number_to_zero.py

--- number_of_steps_to_reduce_a_number_to_zero.py
+++ number_of_steps_to_reduce_a_number_to_zero.py
@@ -1,13 +1,18 @@
 class Solution:
-    def numberOfSteps(self, num: int) -> int:
+    def numberOfSteps(self, num: int) -> int:  # Time: O(log n)
         """
         Given an integer num, return the number of steps to reduce it to zero.
 
         In one step, if the current number is even, you have to divide it by 2,
         otherwise, you have to subtract 1 from it.
 
+        :param int num: integer number to reduce to zero
+        :returns int: number of steps needed to reduce num to zero
 
-        LeetCode Beats 100%
+        Time complexity: O(log n)
+        Space complexity: O(1)
+
+        LeetCode: Beats 100%
         """
         steps = 0
 
