ea05b17461a7a5126f1f24e34c580df071069b95
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-06-08 14:26:20 +0300

    Create lexicographical_numbers.py

---
+++ lexicographical_numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> list[int]:
+        """Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+        Args:
+            n (int): The upper bound of the range [1, n]
+
+        Returns:
+            list[int]: A list of integers from 1 to n sorted in lexicographical order
+
+        Example:
+            >>> lexicalOrder(13)
+            [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        Time complexity: O(n log n) - due to sorting
+        Space complexity: O(n) - for storing the result list
+        """
+        return list(map(int, sorted(map(str, range(1, n + 1)))))
