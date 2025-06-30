45cd47ea50ba0868ccfbbb1be5c7b58f318ad798
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-06-30 13:09:19 +0300

    Update longest_harmonious_subsequence.py

--- longest_harmonious_subsequence.py
+++ longest_harmonious_subsequence.py
@@ -31,3 +31,39 @@ class Solution:
                     max_len = temp
 
         return max_len
+
+
+# Second solution
+class Solution:
+    def findLHS(self, nums: list[int]) -> int:
+        """Finds the length of the longest harmonious subsequence.
+
+        A harmonious list is a list where the difference between its maximum
+        value and its minimum value is exactly 1. This function finds the
+        longest such subsequence in the given `nums` list.
+
+        Args:
+            nums (list[int]): The input list of integers.
+
+        Returns:
+            int: The length of the longest harmonious subsequence, or 0 if
+                 none exists.
+
+        Example:
+            >>> findLHS([1,3,2,2,5,2,3,7])
+            5
+            >>> findLHS([1,2,3,4])
+            2
+            >>> findLHS([1,1,1,1])
+            0
+
+        LeetCode: Beats 99.24% of submissions
+        """
+        c = Counter(nums)
+        longest = 0
+
+        for num, freq in c.items():
+            if c.get(num - 1, -(10**4)) + freq > longest:
+                longest = c.get(num - 1) + freq
+
+        return longest
