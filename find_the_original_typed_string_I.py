594efa027317879ceb7ba99f2336cf60881eb302
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-07-01 13:12:47 +0300

    Create find_the_original_typed_string_I.py

---
+++ find_the_original_typed_string_I.py
@@ -0,0 +1,33 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        """Given a string `word` representing the final output typed by Alice,
+        who may have accidentally held down a key and repeated a character at
+        most once, return the total number of possible original strings Alice
+        could have intended to type.
+
+        Alice may have typed the intended string perfectly, or she may have
+        accidentally repeated a single character (i.e., one group of consecutive
+        identical characters could be the result of a single extra keypress).
+
+        Args:
+            word (str): The string as displayed on Alice's screen.
+
+        Returns:
+
+        Example:
+            >>> possibleStringCount("aabb")
+            3
+            >>> possibleStringCount("abc")
+            1
+            >>> possibleStringCount("aabbc")
+            3
+
+        LeetCode: Beats 100% of submissions
+        """
+        possible_s = 1
+
+        for i in range(1, len(word)):
+            if word[i - 1] == word[i]:
+                possible_s += 1
+
+        return possible_s
