3afe32f89906dd9feb5fba26db1b1488c7a2501f
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-07-18 13:52:24 +0300

    Create keep_multiplying_found_values_by_two.py

---
+++ keep_multiplying_found_values_by_two.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findFinalValue(self, nums: list[int], original: int) -> int:
+        """Given a list of integers `nums` and an integer `original`, repeatedly double `original` as long as it is found in `nums`.
+        Return the final value of `original` after this process.
+
+        Args:
+            nums (list[int]): The list of integers to search within.
+            original (int): The starting integer to search for and double.
+
+        Returns:
+            int: The resulting value of `original` after all possible doublings.
+
+        Example:
+            >>> findFinalValue([5,3,6,1,12], 3)
+            24
+
+        Time complexity: O(n + k), where n is the length of nums and k is the number of doublings.
+        Space complexity: O(n)
+
+        LeetCode: Beats 100% of submissions
+        """
+        nums_d = {num: 1 for num in nums}
+
+        while nums_d.get(original, 0):
+            original *= 2
+
+        return original
