4d2c97a5d689ab8c5f25d69b7a9fc2d1b1a64630
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-08-15 17:20:53 +0300

    Update power_of_four.py

--- power_of_four.py
+++ power_of_four.py
@@ -1,3 +1,4 @@
+# First solution
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         """
@@ -25,3 +26,53 @@ class Solution:
             val *= 4
 
         return val == n
+
+
+# Second solution
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        """
+        Determines if the given integer n is a power of four.
+
+        An integer n is a power of four if there exists an integer x such that n == 4 ** x.
+
+        Args:
+            n (int): The integer to check.
+
+        Returns:
+            bool: True if n is a power of four, False otherwise.
+
+        Example:
+            >>> isPowerOfFour(16)
+            True
+            >>> isPowerOfFour(5)
+            False
+
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+
+        LeetCode: Beats 100% of submissions
+        """
+        if n <= 0:
+            return False
+
+        nums = [
+            1,
+            4,
+            16,
+            64,
+            256,
+            1024,
+            4096,
+            16384,
+            65536,
+            262144,
+            1048576,
+            4194304,
+            16777216,
+            67108864,
+            268435456,
+            1073741824,
+        ]
+
+        return n in nums
