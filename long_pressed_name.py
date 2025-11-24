771577078d6b3aba87e2c898eff8d78f986a10d2
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2025-11-24 22:02:25 +0300

    Update long_pressed_name.py

--- long_pressed_name.py
+++ long_pressed_name.py
@@ -1,35 +1,62 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        i = j = 0
-        letters_n = 1
-        letters_t = 1
-        name += "."
-        typed += "."
+        """
+        Determines if the typed string could be a result of long pressing characters
+        while typing the original name string.
 
-        while i < len(name) - 1:
-            if name[i + 1] == name[i]:
-                letters_n += 1
-                i += 1
-                continue
+        When typing a character, a user might long press a key, causing the same character
+        to appear one or more times in a row. This function checks if the string 'typed'
+        could be the result of such long presses on the string 'name'.
 
-            if typed[j] != name[i]:
+        Args:
+            name (str): The original name string.
+            typed (str): The string produced by possibly long pressing some keys.
+
+        Returns:
+            bool: True if 'typed' could be a result of long pressing characters in 'name',
+            False otherwise.
+
+        Example:
+            Input: name = "alex", typed = "aaleex"
+            Output: True
+
+            Input: name = "saeed", typed = "ssaaedd"
+            Output: False
+
+        Time Complexity: O(N), where N is the length of 'typed'.
+        Space Complexity: O(M), where M is the number of unique character groups.
+
+        LeetCode: Beats 100% of submissions
+        """
+
+        def group_characters(s: str) -> list[list]:
+            prev = s[0]
+            prev_c = 1
+            s_groups = []
+
+            for i in range(len(s)):
+                if s[i] == prev:
+                    prev_c += 1
+                else:
+                    s_groups.append([prev, prev_c])
+                    prev = s[i]
+                    prev_c = 1
+
+            s_groups.append([prev, prev_c])
+
+            return s_groups
+
+        name_groups = group_characters(name)
+        typed_groups = group_characters(typed)
+
+        if len(name_groups) != len(typed_groups):
+            return False
+
+        for i in range(len(name_groups)):
+            if (
+                name_groups[i][0] != typed_groups[i][0]
+                or name_groups[i][1] > typed_groups[i][1]
+            ):
                 return False
-
-            while j < len(typed) - 1 and typed[j] == typed[j + 1]:
-                letters_t += 1
-                j += 1
-
-            if letters_t < letters_n:
-                return False
-
-            letters_t = 1
-            letters_n = 1
-            i += 1
-            j += 1
 
         return True
-
-
-a = Solution()
-
-print(a.isLongPressedName("xnhtq", "xhhttqq"))
